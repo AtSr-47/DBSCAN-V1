e9cc48c76ed041fa3312a5a43a22057dbace4957
Athmane Srai <33730819+AthmaneSrai@users.noreply.github.com>
2019-06-27 16:49:34 +0100

    Add files via upload

---
+++ DBSCAN/src/dbscan/Point.java
@@ -0,0 +1,18 @@
+package dbscan;
+
+public class Point {
+
+    private int x;
+
+    private int y;
+
+    public Point(int a, int b) {
+        x = a;
+        y = b;
+    }
+
+    public int getX() {return x;}
+
+    public int getY() {return y;}
+
+}
